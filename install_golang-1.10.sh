1f3f8c7f1c254d32a9c59f7b571785b32f69262a
Thierry Grangeray <ext.thierry.grangeray@sncf.fr>
2022-03-25 15:30:04 +0100

    golang

---
+++ install_golang-1.10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -O https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+mkdir -p ~/go
+sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
+rm -f go1.10.linux-amd64.tar.gz
+cat >>~/.profile <<x
+# golang
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+x
+source ~/.profile
--- install_vim.sh
+++ install_vim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Installer la font hack
-sudo apt install fonts-hack-ttf
+sudo apt install -y fonts-hack-ttf
 # Installer 'the silver search' (https://github.com/ggreer/the_silver_searcher)
-apt-get install silversearcher-ag
+apt-get install -y silversearcher-ag
 # Installer 'FZF' (https://github.com/junegunn/fzf)
-sudo apt-get install fzf
+sudo apt-get -y install fzf
 # Installer ripgrep (https://github.com/BurntSushi/ripgrep#installation)
-sudo apt-get install ripgrep
+sudo apt-get -y install ripgrep
 # Installer vim
-sudo apt-get install vim
+sudo apt-get -y install vim
