a674e64ceb1008b4168777f9076a2f24f6f07927
Thierry Grangeray <ext.thierry.grangeray@sncf.fr>
2022-03-25 12:29:19 +0100

    golang

--- install_golang.sh
+++ install_golang.sh
@@ -1,8 +1,11 @@
-curl https://go.dev/dl/go1.18.linux-amd64.tar.gz
+curl -O https://storage.googleapis.com/golang/go1.18.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
+mkdir -p ~/go
 sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
 cat >>~/.profile <<x
 # golang
-export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 x
+source ~/.profile
 go install golang.org/x/tools/...@latest .
